2281e754aa398c3ed8980450a3401508308cfd1a
Gustavo Cysneiros <gcc.prof@protonmail.com>
2022-09-13 21:54:48 -0300

    Add base CSS files and static file serving

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const authRoutes = require('./routes/auth.routes');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 app.use(authRoutes);
 
 app.listen(port, () => {
